783f6a526fa92e0f74776c12ebb17322253f5ee3
Tyler Russell <tyler.russell1@aol.com>
2023-11-01 23:47:57 -0500

    Added testing utility.

--- work_cluster/calculate_test.py
+++ work_cluster/calculate_test.py
@@ -1,3 +1,9 @@
 from models import Request
+import numpy as np
 
+THRESHOLD = 10**(-9)
 request = Request([0.2, 0.8], [0.2, 0.8], [0.0, 0.0], [0.0, 0.0], [1.0, 1.0], 0.005, 100)
+
+# returns if two ndarrays are numerically close
+def is_close_ndarray(a, b):
+    return np.linalg.norm(a-b) < THRESHOLD
